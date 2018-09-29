38d11ca5ef11436db507da767f203c07162373c7
mokchenda <mokchend@yahoo.com>
2018-09-29 23:59:34 +0200

    Add SSL support

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('dotenv').config()
+
 var express = require('express');
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
+var sslOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    passphrase: "changeit"
+};
+
 var server = express();
 http.createServer(server).listen(3000);
+https.createServer(sslOptions, server).listen(3443)
+
 
 server.get('/', function (req, res) {
     res.send("Hello World!");
