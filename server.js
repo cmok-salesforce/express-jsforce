e77936371828a70463a30d2702d6e2f3e7323285
mokchenda <mokchend@yahoo.com>
2018-09-30 00:20:41 +0200

    read config from build.properties

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
-const express = require('express');
+var express = require('express');
 const jsforce = require('jsforce');
 const session = require('express-session');
 const config = require('./config');
 const bodyParser = require('body-parser');
-
-var express = require('express');
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
+
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader(process.env.BUILD_PROPERTIE_LOCATION);
 
 var sslOptions = {
     key: fs.readFileSync('key.pem'),
@@ -23,5 +24,5 @@ https.createServer(sslOptions, server).listen(3443)
 
 
 server.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.send("Hello World!" + properties.get('sf.devciam.username'));
 });
